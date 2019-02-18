af859d92f22ac64f888faead1f04f286b82b43e9
luojunjie <luojunjie@inspur.com>
2019-02-18 21:42:31 +0800

    新增脑图

---
+++ src/exception/TryWithSource.java
@@ -0,0 +1,10 @@
+package exception;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class TryWithSource {
+    Vector vector = new Vector();
+    List<String> list = new ArrayList<>();
+}
