edf14c455bc91c03234f5d96f3fc1fff2686f27a
luojunjie <junjie.luo@enmotech.com>
2019-03-20 21:53:57 +0800

    数组和大字段测试

---
+++ src/main/java/array/ArrayTest.java
@@ -0,0 +1,14 @@
+package array;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author: LJJ
+ * @Date: 2019/3/20 13:00
+ */
+public class ArrayTest {
+    @Test
+    public void test1(){
+        int[] array = new int[]{1,2,3,4};
+    }
+}
--- src/main/java/math/BigTest.java
+++ src/main/java/math/BigTest.java
@@ -1,10 +1,19 @@
 package math;
 
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 /**
  * @Author: LJJ
  * @Date: 2019/3/20 12:54
  * 大数值使用
  */
 public class BigTest {
-
+    @Test
+   void test(){
+        BigDecimal bigDecimal = new BigDecimal("0.0");
+        BigInteger bigInteger = new BigInteger("46546465456464646464654654878765456445869498984984949494");
+   }
 }
