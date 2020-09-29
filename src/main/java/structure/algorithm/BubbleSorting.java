cac472ad11a41410e841bb1558559fc6e3ba4731
ljj <junjie.luo@enmotech.com>
2020-09-29 16:44:47 +0800

    修改冒泡排序

--- src/main/java/structure/algorithm/BubbleSorting.java
+++ src/main/java/structure/algorithm/BubbleSorting.java
@@ -13,13 +13,14 @@ import java.util.stream.Stream;
 public class BubbleSorting {
 
     public static void main(String[] args) {
-        Integer[] testArray = {-1,20,50,88,5,4,6,2};
+        Integer[] testArray1 = {-1,20,50,88,5,4,6,2};
         //基本冒泡排序
-        commonBubbleSorting(testArray);
-        System.out.println(Stream.of(testArray).map(String::valueOf).collect(Collectors.joining(",")));
+        commonBubbleSorting(testArray1);
+        System.out.println(Stream.of(testArray1).map(String::valueOf).collect(Collectors.joining(",")));
         //优化冒泡排序
-        optimizedBubbleSorting(testArray);
-        System.out.println(Stream.of(testArray).map(String::valueOf).collect(Collectors.joining(",")));
+        Integer[] testArray2 = {-1,20,50,88,5,4,6,2};
+        optimizedBubbleSorting(testArray2);
+        System.out.println(Stream.of(testArray2).map(String::valueOf).collect(Collectors.joining(",")));
     }
 
     private static void commonBubbleSorting(Integer[] testArray){
@@ -36,13 +37,9 @@ public class BubbleSorting {
     }
 
     private static void optimizedBubbleSorting(Integer[] testArray){
-        boolean flag = false;
+        boolean flag;
         int temp;
         for (int i=0;i<testArray.length-1;++i){
-            //如果循环了一次，都没有发生交换，说明已经有序了，就不用继续排序了
-            if (flag){
-                break;
-            }
             flag = false;
             for (int j=0;j<testArray.length-i-1;++j){
                 if (testArray[j]>testArray[j+1]){
@@ -51,6 +48,10 @@ public class BubbleSorting {
                     testArray[j] = temp;
                     flag = true;
                 }
+            }
+            //如果循环了一次，都没有发生交换，说明已经有序了，就不用继续排序了
+            if (!flag){
+                break;
             }
         }
     }
