a1c2a5801f3e7de9741174e43269a86eb023c02e
ljj <junjie.luo@enmotech.com>
2020-11-06 11:41:39 +0800

    新增基数排序优化

--- src/main/java/structure/algorithm/RadixSortDemo.java
+++ src/main/java/structure/algorithm/RadixSortDemo.java
@@ -1,5 +1,6 @@
 package structure.algorithm;
 
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,7 +14,8 @@ import java.util.stream.Stream;
 public class RadixSortDemo {
     public static void main(String[] args) {
         Integer[] testArray = {15,22,33,1,2,7,5,14,11,95,102,358};
-        radixSort(testArray,10);
+        //radixSort(testArray,10);
+        redixSortOptimize(testArray);
         System.out.println(Stream.of(testArray).map(String::valueOf).collect(Collectors.joining(",")));
     }
 
@@ -46,9 +48,47 @@ public class RadixSortDemo {
             }
         }
         if (hasNext){
-            System.out.println(Stream.of(testArray).map(String::valueOf).collect(Collectors.joining(",")));
             radixSort(testArray,num*10);
         }
     }
 
+    private static void redixSortOptimize(Integer[] testArray){
+        int max = testArray[0];
+        for (int i = 1; i < testArray.length; i++) {
+            if (testArray[i]>max) {
+                max = testArray[i];
+            }
+        }
+        // 个位数: array[i] / 1 % 10 = 3
+        // 十位数：array[i] / 10 % 10 = 9
+        // 百位数：array[i] / 100 % 10 = 5
+        // 千位数：array[i] / 1000 % 10 = ...
+        for (int divider = 1; divider <= max; divider*=10) {
+            // 开辟内存空间，存储次数
+            int[] counts = new int[10];
+            // 统计每个整数出现的次数
+            for (Integer integer : testArray) {
+                counts[integer / divider % 10]++;
+            }
+
+            // 累加次数，统计每个桶最大的索引值，用于后续确认元素位置
+            //累加完后，假如counts[2]=6，说明第三个桶里面的数最大索引为5
+            for (int i = 1; i < counts.length; i++) {
+                counts[i]+= counts[i-1];
+            }
+
+            //从后向前遍历元素，将她放在有序数组中的合适位置
+            int[] newArray = new int[testArray.length];
+            for (int i = testArray.length-1; i >=0 ; i--) {
+                //获取元素在counts中的索引
+                int index = testArray[i]/divider%10;
+                //放在合适位置
+                newArray[--counts[index]] = testArray[i];
+            }
+            // 将有序数组赋值到array
+            for (int i = 0; i < newArray.length; ++i) {
+                testArray[i] = newArray[i];
+            }
+        }
+    }
 }
