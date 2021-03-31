721c6b1691b96a2a0a9a6d3836af3e08c3a1b1a5
ljj <junjie.luo@enmotech.com>
2021-03-31 10:44:06 +0800

    新增future操作

---
+++ src/main/java/concurrency/future/GetException.java
@@ -0,0 +1,45 @@
+package concurrency.future;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * @author ljj
+ * @version sprint 39
+ * @className GetException
+ * @description callable抛出异常，future的isDone()还是会返回false，因为确实只是完毕了
+ * @date 2021-03-30 17:59:25
+ */
+public class GetException {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(20);
+        Future<Integer> future = service.submit(new CallableTask());
+
+
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(i);
+                Thread.sleep(500);
+            }
+            System.out.println(future.isDone());
+            future.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        service.shutdown();
+    }
+
+
+    static class CallableTask implements Callable<Integer> {
+
+        @Override
+        public Integer call() throws Exception {
+            throw new IllegalArgumentException("Callable抛出异常");
+        }
+    }
+}
---
+++ src/main/java/concurrency/future/OneFuture.java
@@ -0,0 +1,39 @@
+package concurrency.future;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * @author ljj
+ * @version sprint 39
+ * @className OneFuture
+ * @description
+ * @date 2021-03-30 17:53:40
+ */
+public class OneFuture {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        Future<Integer> future = service.submit(new CallableTask());
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        service.shutdown();
+    }
+
+    static class CallableTask implements Callable<Integer> {
+
+        @Override
+        public Integer call() throws Exception {
+            Thread.sleep(3000);
+            return new Random().nextInt();
+        }
+    }
+}
